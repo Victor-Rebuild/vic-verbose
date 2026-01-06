39841d43060c33c5f072a9a328af31540b6d1547
Switch-modder <deolrajjyot@gmail.com>
2026-01-06 12:00:54 -0500

    Use /home/raj-jyot instead of raj-jyot for docker compat

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ set -e
 
 TOOLCHAIN_VER="5.3.0-r07"
 
-if [[ ! -d ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER ]]; then
+if [[ ! -d $HOME/.anki/vicos-sdk/dist/$TOOLCHAIN_VER ]]; then
   echo "Getting toolchain version $TOOLCHAIN_VER..."
-  mkdir -p ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
-  cd ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
+  mkdir -p $HOME/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
+  cd $HOME/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
   wget -q --show-progress https://github.com/os-vector/wire-os-externals/releases/download/$TOOLCHAIN_VER/vicos-sdk_"$TOOLCHAIN_VER"_amd64-linux.tar.gz -O - | tar -xz
   echo "Toolchain version $TOOLCHAIN_VER has been installed!"
 else
   echo "Toolchain version $TOOLCHAIN_VER is already installed!"
 fi
 
-TC="/home/$USER/.anki/vicos-sdk/dist/$TOOLCHAIN_VER/prebuilt/bin/arm-oe-linux-gnueabi-"
+TC="$HOME/.anki/vicos-sdk/dist/$TOOLCHAIN_VER/prebuilt/bin/arm-oe-linux-gnueabi-"
 
 cd vector-gobot
 GCC="${TC}clang" GPP="${TC}clang++" make vector-gobot
